bf3915122e49cff95c09cdbd162cc6f99f5d2ce9
Алина Мордюкова <alinavalerievna02@mail.ru>
2026-08-26 20:08:24 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -15,17 +15,16 @@ print(f"Привет, {user_name.title()}!")
 
 # Порядок ввода строго как в тесте: имя, возраст, ВЕС, РОСТ
 user_weight = float(input('Укажите, пожалуйста, вес (в кг, через точку)'))
-user_height = float(
-    input('Пожалуйста, введите свой рост (в м, через точку например: 1.62) '))
+height_prompt = "Пожалуйста, введите свой рост (в м, через точку, например: 1.62): "
+user_height = float(input(height_prompt))
 
 # расчёт индекса массы тела
 bmi = user_weight / (user_height ** 2)
-bmi = round(bmi, 1)
 
 # расчёт нормы воды
 water_ml = user_weight * WATER_REC
 water_l = water_ml / WATER_PER_KG
-print("Отчет для пользователя:", user_name.title(),' (', user_age, 'г.)')
-print("Твой Индекс Массы Тела:", bmi)
-print(f"Рекомендуемая норма воды: {water_l} л. в день")
+print(f"Отчет для пользователя: {user_name.title()} ({user_age} г.)")
+print(f"Твой Индекс Массы Тела: {bmi:.1f}")
+print(f"Рекомендуемая норма воды: {water_l:.1f} л. в день")
 print('Расчет окончен. Будьте здоровы!')
